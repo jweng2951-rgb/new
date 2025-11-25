fa51d86c9502deb2cec7c3091a938cf020c6e17c
jason luoei <jweng2951@gmail.com>
2025-11-25 13:29:27 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ensures relative paths are used for assets
   build: {
     outDir: 'dist',
   },
